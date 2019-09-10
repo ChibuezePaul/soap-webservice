728d7a4a7cacafd08fb1aff4d71e52159c033249
Chibueze Paul <chibueze.paul@longbridgetech.com>
2019-09-10 23:14:57 +0100

    Split XSD Endpoint

---
+++ src/main/java/com/infosys/ci/fiusb/webservice/FIEndpoint.java
@@ -0,0 +1,34 @@
+package com.infosys.ci.fiusb.webservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+/**
+ * Created by mac on 10/09/2019.
+ */
+@Endpoint
+public class FIEndpoint {
+	private static final String NAMESPACE_URI = "http://webservice.fiusb.ci.infosys.com";
+
+    @Autowired
+//    private CustomerUpdateRepository repository;
+	
+	private final Logger logger = LoggerFactory.getLogger ( this.getClass () );
+
+    @PayloadRoot (namespace = NAMESPACE_URI, localPart = "executeService")
+    @ResponsePayload
+    public ExecuteServiceResponse getCustUpdateService(@RequestPayload ExecuteService request) {
+
+        ExecuteServiceResponse response = new ExecuteServiceResponse();
+
+//        response.setExecuteServiceReturn(repository.setCustomAcctDetails());
+		logger.info ( "Request Got : {}",request );
+
+        return response;
+    }
+}
--- src/main/java/com/soapws/mandate/MandateEndpoint.java
+++ src/main/java/com/soapws/mandate/MandateEndpoint.java
@@ -1,26 +1,26 @@
-package com.soapws.mandate;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-
-@Endpoint
-public class MandateEndpoint {
-    private static final String NAMESPACE_URI = "http://webservice.fiusb.ci.infosys.com";
-
-    @Autowired
-    private MandateRepository repository;
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "executeMandateService")
-    @ResponsePayload
-    public ExecuteServiceResponse getMandateService(@RequestPayload ExecuteMandateService request) {
-
-        ExecuteServiceResponse response = new ExecuteServiceResponse();
-
-        response.setExecuteServiceReturn(repository.setCustomAcctDetails());
-
-        return response;
-    }
-}
+//package com.soapws.mandate;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.ws.server.endpoint.annotation.Endpoint;
+//import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+//import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+//import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+//
+//@Endpoint
+//public class MandateEndpoint {
+//    private static final String NAMESPACE_URI = "http://webservice.fiusb.ci.infosys.com";
+//
+//    @Autowired
+//    private MandateRepository repository;
+//
+//    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "executeMandateService")
+//    @ResponsePayload
+//    public ExecuteServiceResponse getMandateService(@RequestPayload ExecuteMandateService request) {
+//
+//        ExecuteServiceResponse response = new ExecuteServiceResponse();
+//
+//        response.setExecuteServiceReturn(repository.setCustomAcctDetails());
+//
+//        return response;
+//    }
+//}
